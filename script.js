678eae3a9ebb6175276a4b87e2dcd1ed961130b0
lelido <17890168+lelido@users.noreply.github.com>
2025-03-31 19:25:36 +0900

    Add operate function

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
+function operate(operator, operand1, operand2) {
+    let result;
+
+    switch (operator) {
+        case "+":
+            result = add(operand1, operand2);
+            break;
+        case "-":
+            result = subtract(operand1, operand2);
+            break;
+        case "*":
+            result = multiply(operand1, operand2);
+            break;
+        case "/":
+            result = divide(operand1, operand2);
+            break;
+        default:
+            result = `Wrong operator: ${operator}`;
+    }
+
+    return result;
+}
+
 function add(operand1, operand2) {
     return operand1 + operand2;
 }

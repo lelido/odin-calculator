c4d86337645265a1cab03e30600fec163e88fb3a
lelido <17890168+lelido@users.noreply.github.com>
2025-04-01 11:57:10 +0900

    Add the functions that populate the main display
    When you click a digit button, it appears on the display.
    There is a max length for the number and overflow is hidden. Clear and
    backspace buttons also work.

--- script.js
+++ script.js
@@ -1,3 +1,37 @@
+const displayMain = document.querySelector("#display #main");
+
+document.querySelector("#calculator").addEventListener("click", event => {
+    if (event.target.classList.contains("digit")) {
+        populateMainDisplay(event.target.textContent);
+    } else if (event.target.id === "clear") {
+        clearMainDisplay();
+    } else if (event.target.id === "backspace") {
+        removeLastDigit();
+    }
+});
+
+function populateMainDisplay(digit) {
+    if (displayMain.textContent === "0") {
+        displayMain.textContent = "";
+    }
+
+    if (displayMain.textContent.length < 13) {
+        displayMain.textContent += digit;
+    }
+}
+
+function clearMainDisplay() {
+    displayMain.textContent = "0";
+}
+
+function removeLastDigit() {
+    displayMain.textContent = displayMain.textContent.slice(0, -1);
+
+    if (displayMain.textContent === "") {
+        displayMain.textContent = "0";
+    }
+}
+
 function operate(operator, operand1, operand2) {
     let result;
 
